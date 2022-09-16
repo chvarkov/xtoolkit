0dfe9d0de19d1022b075c61182d930c22bf22343
chvarkov <chvarkov.alexey@gmail.com>
2022-09-16 00:20:28 +0000

    feat: button for query sc.

--- src/app/core/elrond/services/sc-query-runner.ts
+++ src/app/core/elrond/services/sc-query-runner.ts
@@ -1,12 +1,11 @@
 import { Injectable } from '@angular/core';
 import { INetworkEnvironment } from '../interfaces/network-environment';
 import { Address } from '@elrondnetwork/erdjs-network-providers/out/primitives';
-import { SmartContract } from '@elrondnetwork/erdjs/out';
+import { Query, SmartContract } from '@elrondnetwork/erdjs/out';
 import { ScArgsBuilder } from '../builders/sc-args.builder';
+import { ContractQueryResponse, ProxyNetworkProvider } from '@elrondnetwork/erdjs-network-providers/out';
 
 export interface IScQueryOptions {
-	network: INetworkEnvironment,
-	sc: SmartContract,
 	functionName: string,
 	caller?: Address,
 	payload: Record<string, any>,
@@ -14,12 +13,25 @@ export interface IScQueryOptions {
 
 @Injectable({providedIn: 'root'})
 export class ScQueryRunner {
-	query(sc: SmartContract, options: IScQueryOptions): any {
-		sc.createQuery({
+	getProxy(network: INetworkEnvironment): ProxyNetworkProvider {
+		return new ProxyNetworkProvider(network.gatewayUrl);
+	}
+
+	createQuery(sc: SmartContract, options: IScQueryOptions): Query {
+		return sc.createQuery({
 			value: '0',
 			caller: options.caller,
-			func: sc.getEndpoint(options.functionName),
+			func: {
+				name: options.functionName,
+				toString(): string {
+					return options.functionName;
+				}
+			},
 			args: new ScArgsBuilder(sc).build(options.functionName, options.payload),
 		});
+	}
+
+	runQuery(network: INetworkEnvironment, query: Query): Promise<ContractQueryResponse> {
+		return this.getProxy(network).queryContract(query);
 	}
 }
--- src/app/sc-viewer/components/sc-viewer/sc-endpoint/sc-endpoint.component.ts
+++ src/app/sc-viewer/components/sc-viewer/sc-endpoint/sc-endpoint.component.ts
@@ -1,7 +1,12 @@
 import { Component, forwardRef, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { ScInputComponent } from './sc-input/sc-input.component';
-import { EndpointDefinition } from '@elrondnetwork/erdjs/out';
+import { EndpointDefinition, SmartContract } from '@elrondnetwork/erdjs/out';
+import { Observable, of, Subject } from 'rxjs';
+import { ScQueryRunner } from '../../../../core/elrond/services/sc-query-runner';
+import { INetworkEnvironment } from '../../../../core/elrond/interfaces/network-environment';
+import { Store } from '@ngrx/store';
+import { NetworkSelector } from '../../../../network/store/network.selector';
 
 @Component({
 	selector: 'app-sc-endpoint',
@@ -18,11 +23,20 @@ import { EndpointDefinition } from '@elrondnetwork/erdjs/out';
 export class ScEndpointComponent implements OnInit {
 	isShowing = false;
 
+	@Input() sc!: SmartContract;
+
 	@Input() endpoint?: EndpointDefinition;
 
 	form!: FormGroup;
 
-	constructor(private readonly fb: FormBuilder) {
+	resultSubject = new Subject();
+
+	network$: Observable<INetworkEnvironment>;
+
+	constructor(private readonly fb: FormBuilder,
+				private readonly store: Store,
+				private readonly scQueryRunner: ScQueryRunner) {
+		this.network$ = this.store.select(NetworkSelector.selectedNetwork);
 	}
 
 	ngOnInit(): void {
@@ -45,5 +59,20 @@ export class ScEndpointComponent implements OnInit {
 		const args = (this.endpoint?.input || []).map(i => `${i.name}: ${i.type}`);
 
 		return `(${args.join(', ')})`;
+	}
+
+	async sendQuery(network: INetworkEnvironment): Promise<void> {
+		if (!this.endpoint) {
+			return;
+		}
+
+		const query = this.scQueryRunner.createQuery(this.sc, {
+			payload: this.form.value,
+			functionName: this.endpoint.name,
+		});
+
+		const queryResult = await this.scQueryRunner.runQuery(network, query);
+
+		this.resultSubject.next(queryResult.toJSON());
 	}
 }
