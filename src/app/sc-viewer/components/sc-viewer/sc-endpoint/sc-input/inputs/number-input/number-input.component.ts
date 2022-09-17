fd4d4d7641ba70f68c6e168f1de0d9e9b78bab8f
chvarkov <chvarkov.alexey@gmail.com>
2022-09-17 23:10:34 +0000

    feat: min/max for number input.

--- src/app/sc-viewer/components/sc-viewer/sc-endpoint/sc-input/inputs/number-input/number-input.component.ts
+++ src/app/sc-viewer/components/sc-viewer/sc-endpoint/sc-input/inputs/number-input/number-input.component.ts
@@ -1,5 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { SelectElement } from '../../../../../../../core/ui/select/select.component';
+import { Component, Input, OnInit } from '@angular/core';
+import {
+	BigIntType,
+	BigUIntType,
+	I16Type,
+	I32Type,
+	I64Type,
+	I8Type,
+	Type,
+	U16Type,
+	U32Type,
+	U64Type,
+	U8Type
+} from '@elrondnetwork/erdjs/out';
+import BigNumber from 'bignumber.js';
 
 @Component({
 	selector: 'app-number-input',
@@ -7,15 +20,73 @@ import { SelectElement } from '../../../../../../../core/ui/select/select.compon
 	styleUrls: ['./number-input.component.scss']
 })
 export class NumberInputComponent implements OnInit {
-	default: SelectElement = {
-		name: 'dec',
-		value: 'dec',
-	};
+	readonly maxLength = 39;
+
+	@Input() type?: Type;
 
 	constructor() {
 	}
 
+	value: BigNumber = new BigNumber(0);
+
 	ngOnInit(): void {
 	}
 
+	getMin(): BigNumber {
+		if (!this.type) {
+			return new BigNumber(0);
+		}
+
+		switch (true) {
+			case this.type instanceof I8Type:
+				return new BigNumber(2).pow(8).dividedBy(2).minus(1);
+			case this.type instanceof I16Type:
+				return new BigNumber(2).pow(16).dividedBy(2).minus(1);
+			case this.type instanceof I32Type:
+				return new BigNumber(2).pow(32).dividedBy(2).minus(1);
+			case this.type instanceof I64Type:
+				return new BigNumber(2).pow(64).dividedBy(2).minus(1);
+			case this.type instanceof BigIntType:
+				return new BigNumber(2).pow(128).dividedBy(2).minus(1);
+			case this.type instanceof U8Type:
+			case this.type instanceof U16Type:
+			case this.type instanceof U32Type:
+			case this.type instanceof U64Type:
+			case this.type instanceof BigUIntType:
+				return new BigNumber(0);
+			default:
+				throw new Error('input type is not numerical');
+		}
+	}
+
+	getMax(): BigNumber {
+		if (!this.type) {
+			return new BigNumber(0);
+		}
+
+		switch (true) {
+			case this.type instanceof I8Type:
+				return new BigNumber(2).pow(8).dividedBy(2);
+			case this.type instanceof I16Type:
+				return new BigNumber(2).pow(16).dividedBy(2);
+			case this.type instanceof I32Type:
+				return new BigNumber(2).pow(32).dividedBy(2);
+			case this.type instanceof I64Type:
+				return new BigNumber(2).pow(64).dividedBy(2);
+			case this.type instanceof BigIntType:
+				return new BigNumber(2).pow(128).dividedBy(2);
+			case this.type instanceof U8Type:
+				return new BigNumber(2).pow(8);
+			case this.type instanceof U16Type:
+				return new BigNumber(2).pow(16);
+			case this.type instanceof U32Type:
+				return new BigNumber(2).pow(32);
+			case this.type instanceof U64Type:
+				return new BigNumber(2).pow(64);
+			case this.type instanceof BigUIntType:
+				return new BigNumber(2).pow(128);
+			default:
+				throw new Error('input type is not numerical');
+		}
+	}
 }
