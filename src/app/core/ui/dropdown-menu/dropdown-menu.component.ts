96072bd3f2e416e8c6400bd05e33bfe21617085b
chvarkov <chvarkov.alexey@gmail.com>
2022-09-24 21:51:32 +0000

    feat: added dropdown menu.

--- src/app/core/ui/dropdown-menu/dropdown-menu.component.ts
+++ src/app/core/ui/dropdown-menu/dropdown-menu.component.ts
@@ -1,21 +1,32 @@
-import { Component, ElementRef, HostListener, Input, OnInit, ViewChild } from '@angular/core';
+import {
+	Component,
+	ContentChildren,
+	ElementRef,
+	HostListener,
+	Input,
+	QueryList,
+	ViewChild
+} from '@angular/core';
+import { OptionComponent } from '../select/option/option.component';
+import { DropdownMenuItemComponent } from './dropdown-menu-item/dropdown-menu-item.component';
 
 @Component({
 	selector: 'app-dropdown-menu',
 	templateUrl: './dropdown-menu.component.html',
 	styleUrls: ['./dropdown-menu.component.scss']
 })
-export class DropdownMenuComponent implements OnInit {
+export class DropdownMenuComponent {
+	@ContentChildren(OptionComponent) options?: QueryList<DropdownMenuItemComponent>;
 	@ViewChild('dropdown', {static: true}) selfRef?: ElementRef;
 
 	isOpen = false;
 
-	@Input() menuPosition:'left' | 'right' = 'right';
+	@Input() horizontal:'left' | 'right' = 'right';
+	@Input() vertical: 'top' | 'bottom' = 'bottom';
 
-	constructor() {
-	}
-
-	ngOnInit(): void {
+	switchShowOptions(e: Event): void {
+		e.stopPropagation();
+		this.isOpen = !this.isOpen;
 	}
 
 	@HostListener('document:click', ['$event'])
