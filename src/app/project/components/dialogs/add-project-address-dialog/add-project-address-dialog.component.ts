2e9f1abc1ebd2cbc682bd4b66e40d07828fe9cf5
chvarkov <chvarkov.alexey@gmail.com>
2022-10-01 14:29:31 +0000

    fix: removed hardcoded chainId in add address dialog.

--- src/app/project/components/dialogs/add-project-address-dialog/add-project-address-dialog.component.ts
+++ src/app/project/components/dialogs/add-project-address-dialog/add-project-address-dialog.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AbstractModalDialog } from '../../../../core/ui/dialog/abstract-modal-dialog';
 import { DialogRef } from '../../../../core/ui/dialog/dialog-ref';
 import { Observable } from 'rxjs';
-import { ProjectAbi, ProjectAddress } from '../../../../core/data-provider/data-provider';
+import { Project, ProjectAbi, ProjectAddress } from '../../../../core/data-provider/data-provider';
 import { Store } from '@ngrx/store';
 import { ProjectSelector } from '../../../store/project.selector';
 import { map } from 'rxjs/operators';
@@ -21,6 +21,8 @@ export class AddProjectAddressDialogComponent extends AbstractModalDialog implem
 
 	abiInterfaces$?: Observable<ProjectAbi[]>;
 
+	project$?: Observable<Project | undefined>;
+
 	constructor(private readonly store: Store) {
 		super();
 	}
@@ -29,6 +31,8 @@ export class AddProjectAddressDialogComponent extends AbstractModalDialog implem
 		this.abiInterfaces$ = this.store.select(ProjectSelector.projectById(this.dialogRef.data.projectId)).pipe(
 			map(project => project?.abiInterfaces || []),
 		);
+
+		this.project$ = this.store.select(ProjectSelector.projectById(this.dialogRef.data.projectId));
 
 		this.dialogRef.options.width = '560px';
 		this.dialogRef.options.height = '260px';
