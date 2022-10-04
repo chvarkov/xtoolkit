773f0315234b7015028c92bd61217e4f358bef0e
chvarkov <chvarkov.alexey@gmail.com>
2022-10-04 22:06:41 +0000

    feat: toolbar title size option.

--- src/app/core/ui/toolbar/toolbar.component.ts
+++ src/app/core/ui/toolbar/toolbar.component.ts
@@ -1,11 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+export type ToolBarTitleSize = 'big' | 'medium' | 'small';
+
 @Component({
 	selector: 'app-toolbar',
 	templateUrl: './toolbar.component.html',
 	styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent implements OnInit {
+	@Input() titleSize: ToolBarTitleSize = 'big';
 	@Input() title?: string
 
 	constructor() {
