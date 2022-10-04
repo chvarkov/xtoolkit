66d3177b981c96d2067a3ae64d0d437d24db709c
chvarkov <chvarkov.alexey@gmail.com>
2022-10-04 22:58:36 +0000

    fix: removed unused imports.

--- src/app/project/components/dialogs/generate-wallet-dialog/generate-wallet-dialog.component.ts
+++ src/app/project/components/dialogs/generate-wallet-dialog/generate-wallet-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DialogRef } from '../../../../core/ui/dialog/dialog-ref';
 import { Mnemonic } from '@elrondnetwork/erdjs-walletcore/out';
 import { AbstractModalDialog } from '../../../../core/ui/dialog/abstract-modal-dialog';
