91646427860b47f33471e4e606e5e13bd7afdac2
chvarkov <chvarkov.alexey@gmail.com>
2022-12-25 03:44:02 +0000

    fix: await tx completed.

--- src/app/core/elrond/helpers/parse-tx-status.ts
+++ src/app/core/elrond/helpers/parse-tx-status.ts
@@ -2,13 +2,19 @@ import { ITransactionOnNetwork } from '@elrondnetwork/erdjs/out';
 import { TransactionStatus } from '../enums/transaction-status';
 
 export function parseTxStatus(tx: ITransactionOnNetwork): TransactionStatus {
-	const statusObj = tx.status as unknown as {status: TransactionStatus};
+	if (tx.status.isPending()) {
+		return TransactionStatus.Pending;
+	}
 
-	if (statusObj.status === TransactionStatus.Fail) {
+	if (tx.status.isFailed() || tx.status.isInvalid()) {
 		return TransactionStatus.Fail;
 	}
 
-	const errorSignal = tx.logs.findEvents('errorSignal');
+	const errorSignals = tx.logs.findEvents('signalError');
 
-	return errorSignal ? TransactionStatus.Fail : TransactionStatus.Success;
+	if (errorSignals.length) {
+		return TransactionStatus.Fail;
+	}
+
+	return TransactionStatus.Success
 }
