ab000cb51eb46c0560529f20ef19a56d2d99b264
chvarkov <chvarkov.alexey@gmail.com>
2022-12-26 02:06:51 +0000

    feat: impl value accessor to bytes input.

--- src/app/tabs-viewer/components/sc-viewer/sc-endpoint/sc-input/inputs/bytes-input/bytes-input.component.ts
+++ src/app/tabs-viewer/components/sc-viewer/sc-endpoint/sc-input/inputs/bytes-input/bytes-input.component.ts
@@ -1,17 +1,75 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, forwardRef, Input, OnInit, Output } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+export type ByteEncodingType = 'base64' | 'hex' | 'utf-8';
 
 @Component({
 	selector: 'app-bytes-input',
 	templateUrl: './bytes-input.component.html',
-	styleUrls: ['./bytes-input.component.scss']
+	styleUrls: ['./bytes-input.component.scss'],
+	providers: [
+		{
+			provide: NG_VALUE_ACCESSOR,
+			useExisting: forwardRef(() => BytesInputComponent),
+			multi: true,
+		},
+	],
 })
-export class BytesInputComponent implements OnInit {
+export class BytesInputComponent implements OnInit, ControlValueAccessor {
 	@Output() changed: EventEmitter<Buffer> = new EventEmitter<Buffer>();
+	@Input() cols = 200;
+	@Input() rows = 2;
+	@Input() encodingMethods: ByteEncodingType[] = ['utf-8', 'hex', 'base64'];
+	@Input() disabled = false;
+
+	method: ByteEncodingType = 'utf-8';
+
+	private onChange: Function = () => null;
+	private onTouch: Function = () => null;
+	val: Buffer = Buffer.from([]);
+
+	stringValue: string = '';
 
 	constructor() {
 	}
 
-	ngOnInit(): void {
+	ngOnInit() {
 	}
 
+	setDisabledState(isDisabled: boolean): void {
+		this.disabled = isDisabled;
+	}
+
+	onChangeEncodingMethod(v: string): void {
+		this.method = v as ByteEncodingType;
+
+		this.onChange(this.val);
+		this.onTouch();
+	}
+
+	set value(val: Buffer) {
+		this.val = val;
+		this.onChange(val);
+		this.onTouch();
+	}
+
+	writeValue(value: Buffer) {
+		this.val = value;
+	}
+
+	registerOnChange(fn: any) {
+		this.onChange = fn;
+	}
+
+	registerOnTouched(fn: any) {
+		this.onTouch = fn;
+	}
+
+	onChangedValue(e: Event): void {
+		const data = (<HTMLTextAreaElement>e.target).value;
+		this.val = Buffer.from(data, this.method);
+		this.onChange(this.val);
+		this.onTouch();
+		this.changed.emit(this.val);
+	}
 }
