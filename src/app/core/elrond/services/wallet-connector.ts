41f47941018b8fecef56bbd096e5019687b12dc1
chvarkov <chvarkov.alexey@gmail.com>
2023-01-04 21:09:21 +0000

    feat: sign tx via maiar app.

--- src/app/core/elrond/services/wallet-connector.ts
+++ src/app/core/elrond/services/wallet-connector.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { WalletConnectProvider } from '@elrondnetwork/erdjs-wallet-connect-provider/out';
-import { Observable, Subject } from 'rxjs';
+import { Observable, Subject, throwError } from 'rxjs';
 import { fromPromise } from 'rxjs/internal-compatibility';
 import { filter, map, switchMap, take } from 'rxjs/operators';
 import { DEFAULT_WALLET_CONNECT_BRIDGE_URL } from '../../constants';
+import { Transaction } from '@elrondnetwork/erdjs/out';
 
 export class WalletLoginRef {
 	constructor(public readonly connectUrl: string,
@@ -41,6 +42,22 @@ export class WalletConnector {
 
 	get activeAddress$(): Observable<string | undefined> {
 		return this.activeAddressSubject.asObservable();
+	}
+
+	get activeAddress(): string {
+		return this.provider.address;
+	}
+
+	signTransaction(tx: Transaction): Observable<void> {
+		return fromPromise(this.provider.isConnected()).pipe(
+			switchMap(isConnected => {
+				if (!isConnected) {
+					return throwError(new Error('Wallet is not connected'));
+				}
+
+				return fromPromise(this.provider.signTransaction(tx)).pipe(map(() => undefined));
+			}),
+		);
 	}
 
 	createLogin(walletConnectBridge: string): Observable<WalletLoginRef> {
