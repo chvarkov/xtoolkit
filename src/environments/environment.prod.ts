2c19a1364b44452936009bfcdd78ab71e769b5ed
chvarkov <chvarkov.alexey@gmail.com>
2023-01-11 00:14:43 +0000

    feat: updated github links.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
 	production: true,
 	apiUrl: '',
-	githubLink: 'https://github.com/chvarkov/xdebug',
-	githubIssuesLink: 'https://github.com/chvarkov/xdebug/issues',
+	githubLink: 'https://github.com/chvarkov/dapplab',
+	githubIssuesLink: 'https://github.com/chvarkov/dapplab/issues',
 	donationAddress: 'erd1hfq692d2rd9ft6r35r9yj6quhya4vydzqjgmwcvfq063amfmaufqqlu6fh',
 	donationHerotag: 'alexey',
 	feedbackFormUrl: 'https://docs.google.com/forms/d/e/1FAIpQLSchnzfh42IOH41-vP3d2Nfgze8ym00Ky3zVt2QFO6I0OrKhMw/viewform?usp=sf_link',
