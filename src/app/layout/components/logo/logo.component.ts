8d005d5f9cefc2f913b537b85c6189cf1fe4db5d
chvarkov <chvarkov.alexey@gmail.com>
2023-01-12 00:45:44 +0000

    feat: updated display active project/network.

--- src/app/layout/components/logo/logo.component.ts
+++ src/app/layout/components/logo/logo.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
 	styleUrls: ['./logo.component.scss']
 })
 export class LogoComponent implements OnInit {
-	@Input() networkName: string = ''
+	@Input() projectName?: string;
 
 	constructor() {
 	}
