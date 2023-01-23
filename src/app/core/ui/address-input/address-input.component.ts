f549a2804b5d53a074d9624163f8b2ae2f5d84de
chvarkov <chvarkov.alexey@gmail.com>
2023-01-23 19:16:52 +0000

    feat: address input as value accessor.

--- src/app/core/ui/address-input/address-input.component.ts
+++ src/app/core/ui/address-input/address-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, forwardRef, Input, OnInit, Output } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { INetworkEnvironment } from '../../elrond/interfaces/network-environment';
 import { Observable, of } from 'rxjs';
@@ -8,21 +8,46 @@ import { ProjectAddress } from '../../data-provider/data-provider';
 import { ProjectSelector } from '../../../project/store/project.selector';
 import { map } from 'rxjs/operators';
 import { ClipboardService } from '../../services/clipboard.service';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import BigNumber from 'bignumber.js';
 
 @Component({
 	selector: 'app-address-input',
 	templateUrl: './address-input.component.html',
-	styleUrls: ['./address-input.component.scss']
+	styleUrls: ['./address-input.component.scss'],
+	providers: [
+		{
+			provide: NG_VALUE_ACCESSOR,
+			useExisting: forwardRef(() => AddressInputComponent),
+			multi: true,
+		},
+	],
 })
-export class AddressInputComponent implements OnInit {
+export class AddressInputComponent implements OnInit, ControlValueAccessor {
 	@Input() projectId: string = ''
 	@Input() chainId: string = ''
-	@Input() address: string = '';
 	@Input() type?: 'sc' | 'wallet';
 	@Input() options: ProjectAddress[] = [];
+	@Input() showAddressBook = true;
+	@Input() set value(val: string) {
+		this.onChange(val);
+		this.onTouch(val);
+		this.val = val;
+	}
+
+	get value(): string {
+		return this.val;
+	}
+
+	val: string = '';
+
 	@Output() changed: EventEmitter<string> = new EventEmitter<string>();
 
-	@Input() showAddressBook = true;
+
+	onChange: any = () => {};
+	onTouch: any = () => {};
+
+	isDisabled = false;
 
 	isOptionsVisible = false;
 
@@ -53,7 +78,7 @@ export class AddressInputComponent implements OnInit {
 	}
 
 	explore(url: string): void {
-		window.open(`${url}/accounts/${this.address}`, '_blank');
+		window.open(`${url}/accounts/${this.value}`, '_blank');
 	}
 
 	isInvalidAddress(address: string): boolean {
@@ -66,10 +91,12 @@ export class AddressInputComponent implements OnInit {
 		this.setValue(value);
 	}
 
-	onSelectAddress(savedAddress: ProjectAddress): void {
-		this.setValue(savedAddress.address);
+	onChangeEvent(e: Event): void {
+		this.val = (<HTMLInputElement>e.target).value;
 
-		this.address = savedAddress.address;
+		this.changed.emit(this.val);
+		this.onChange(this.val);
+		this.onTouch();
 
 		this.isOptionsVisible = false;
 	}
@@ -79,5 +106,21 @@ export class AddressInputComponent implements OnInit {
 			this.prevValue = value;
 			this.changed.next(value);
 		}
+	}
+
+	writeValue(value: any){
+		this.value = value
+	}
+
+	registerOnChange(fn: any){
+		this.onChange = fn
+	}
+
+	registerOnTouched(fn: any){
+		this.onTouch = fn
+	}
+
+	setDisabledState(isDisabled: boolean) {
+		this.isDisabled = isDisabled;
 	}
 }
